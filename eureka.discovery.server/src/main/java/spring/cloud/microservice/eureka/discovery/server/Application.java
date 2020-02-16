9ed2fe16f781910a4a307bb0488dbb31ea6272de
gcintir <guraycintir@gmail.com>
2020-02-16 19:17:51 +0300

    initial status

--- eureka.discovery.server/src/main/java/spring/cloud/microservice/eureka/discovery/server/Application.java
+++ eureka.discovery.server/src/main/java/spring/cloud/microservice/eureka/discovery/server/Application.java
@@ -2,7 +2,9 @@ package spring.cloud.microservice.eureka.discovery.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class Application {
 
