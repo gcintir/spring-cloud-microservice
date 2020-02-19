8f3e665fee5375150505767b7fa7e22cb7ab0b6a
gcintir <guraycintir@gmail.com>
2020-02-19 23:49:09 +0300

    updated properties

---
+++ zuul.api.gateway/src/main/java/spring/cloud/microservice/zuul/api/gateway/AppConfiguration.java
@@ -0,0 +1,10 @@
+package spring.cloud.microservice.zuul.api.gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class AppConfiguration {
+
+}
