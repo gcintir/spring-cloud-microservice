c664e3a3c9cab5bc4d974815253daade7a853632
gcintir <guraycintir@gmail.com>
2020-03-03 22:07:32 +0300

    config server

--- account.service/src/main/java/spring/cloud/microservice/account/service/AccountController.java
+++ account.service/src/main/java/spring/cloud/microservice/account/service/AccountController.java
@@ -1,6 +1,7 @@
 package spring.cloud.microservice.account.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/account")
 public class AccountController {
 
+    @Value("${account.id}")
+    private String accountId;
+
     @Autowired
     private Environment environment;
 
@@ -18,5 +22,11 @@ public class AccountController {
     public ResponseEntity<String> status() {
 
         return ResponseEntity.ok().body("account service working on port " + environment.getProperty("local.server.port"));
+    }
+
+    @GetMapping(path = "/info")
+    public ResponseEntity<String> accountId() {
+
+        return ResponseEntity.ok().body("accountId: " + accountId);
     }
 }
